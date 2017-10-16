fe89bc2759f803e20dc2f31edf538a7e1660ab8f
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-10-16 13:24:02 +0900

    array配列の表示
    array配列を逆順に表示するようにスクリプトを作成しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss {
+	private int hp = 100;
+	private int power = 25;
+
+	public void Attack() {
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+
+	public void Defence (int damage) {
+		Debug.Log (damage + "のダメージを受けた");
+
+		this.hp -= damage;
+	}
+}
+
+public class Test : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		Boss lastboss = new Boss ();
+
+		lastboss.Attack ();
+		lastboss.Defence (3);
+
+		Boss midboss = new Boss ();
+
+		midboss.Attack ();
+		midboss.Defence (9);
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/array.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class array : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = { 50, 60, 40, 70, 10 };
+
+		for (int i = array.Length-1; 0 <= i; i--) {
+
+			Debug.Log (array [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
